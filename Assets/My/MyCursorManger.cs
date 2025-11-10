409b11087bff6bd87685118583f7c1344c957184
Wei <ha271923@gmail.com>
2025-11-10 23:36:09 +0800

    fix no cursor in LoseScene, Adjust Ammo, projectile, scene

---
+++ Assets/My/MyCursorManger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCursorManger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+
+}
